086db00f2af43f7ad575e1a67d05338ff98fa2b5
kyleromarate <kyleromarate016@gmail.com>
2024-03-06 03:27:47 +0800

    First commit

--- _helpers/db.js
+++ _helpers/db.js
@@ -1,5 +1,5 @@
 const config = require('config.json');
-const mysql = require('mysql/promise');
+const mysql = require('mysql2/promise');
 const { Sequelize } = require('sequelize');
 
 module.exports = db = {};
--- users/users.controller.js
+++ users/users.controller.js
@@ -25,7 +25,7 @@ function getAll(req, res, next) {
 
 function getById(req, res, next) {
     userService.getById(req.params.id)
-    .then(user => res.json(user))
+    .then(user => res.json(users))
     .catch(next);
 }
 
@@ -41,6 +41,11 @@ function update(req, res, next) {
         .catch(next);     
 }
 
+function _delete(req, res, next){
+    userService.delete(req.params.id)
+    .then(() => res.json({ message: 'User deleted'}))
+}
+
 //schema functions
 
 function createSchema(req, res, next) {
@@ -49,6 +54,24 @@ function createSchema(req, res, next) {
         firstName: Joi.string().required(),
         lastName: Joi.string().required(),
         role: Joi.string().valid(Role.Admin, Role.User).required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).required()
     
-    })
+    });
+    validateRequest(req, next, schema);
+
+}
+
+function updateSchema(req, res, next) {
+    const schema = Joi.object({
+        title: Joi.string().empty(''),
+        firstName: Joi.string().empty(''),
+        lastName: Joi.string().empty(''),
+        role: Joi.string().valid(Role.Admin, Role.User).empty(''),
+        email: Joi.string().email().empty(''),
+        password: Joi.string().min(6).empty(''),
+        confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
+    }).with('password', 'confirmPassword');
+    validateRequest(req, next, schema);
 }
